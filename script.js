6e4aea4d7e678f3c3084c58f6efeed5a27a69972
lweine01 <laurarweiner@gmail.com>
2021-08-20 15:42:45 -0400

    Updated all if statements to be reflect the character they represent.

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var confirmUpperCase = false;
 var confirmSpecial = false;
 var confirmNumbers = false;
 
+
 function generatePassword() {
   var length = prompt("How many characters do you want your password?");
   if (length < 8 || length > 128 ) {
@@ -17,24 +18,46 @@ function generatePassword() {
   }
 
   var password = "";
+  var possibleChoices = "",
 
-  var confirmLowerCase = confirm("Do you want lowerCase?");
+  var confirmLowerCase = confirm("Do you want Lower Case letters in your password?");
   if (confirmLowerCase) {
     var random = Math.floor(Math.random () * lowerCase.length);
     var lowerCaseChoice = lowerCase[random];
     storedValue.push(lowerCaseChoice);
-    console.log (lowerCaseChoice);
+    possibleChoices = possibleChoices.concat(lowerCase);
+  }
+
+  var confirmUpperCase = confirm("Do you want Upper Case letters in your password?");
+  if (confirmUpperCase) {
+    var random = Math.floor(Math.random () * upperCase.length);
+    var upperCaseChoice = upperCase[random];
+    storedValue.push(upperCaseChoice);
+    possibleChoices = possibleChoices.concat(upperCase);
+  }
+
+  var confirmSpecial = confirm("Do you want any special characters in your password?");
+  if (confirmSpecial) {
+    var random = Math.floor(Math.random () * specialChar.length);
+    var specialChoice = specialChar[random];
+    storedValue.push(specialChoice);
+    possibleChoices = possibleChoices.concat(specialChar);
+  }
+
+  var confirmNumbers = confirm("Do you want numbers in your password?");
+  if (confirmNumbers) {
+    var random = Math.floor(Math.random () * numbers.length);
+    var numbersChoice = numbers[random];
+    storedValue.push(numbersChoice);
+    possibleChoices = possibleChoices.concat(numbers);
   }
 
   for (var i=0; i < length; i++) {
-    var random = Math.floor(Math.random() * lowerCase.length);
+    var random = Math.floor(Math.random() * possibleChoices.length);
     password += lowerCase[random];
   }
 
-  var upperCase = confirm ("Do you want upperCase?")
-  if (upperCase) {
-    possiblechoices 
-  }
+
     
   
 
